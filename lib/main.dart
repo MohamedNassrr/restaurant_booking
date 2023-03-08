db9bbc678eac143636c32b464648bd01f0b5bf2f
MohamedNassrr <mohamednasrrkh@gmail.com>
2023-03-08 16:58:22 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restaurant_booking_app/bloc_observer.dart';
+import 'package:restaurant_booking_app/layout/home_cubit/cubit.dart';
+import 'package:restaurant_booking_app/layout/home_cubit/state.dart';
+import 'package:restaurant_booking_app/modules/login_module/cubit/logincubit.dart';
 import 'package:restaurant_booking_app/modules/login_module/login_screen.dart';
 import 'package:restaurant_booking_app/shared/style/theme.dart';
 
@@ -17,10 +20,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      home: LoginScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => HomeCubit(),
+        ),
+        BlocProvider(
+            create: (context) => LoginCubit(),
+        ),
+      ],
+      child: BlocConsumer<HomeCubit, HomeStates>(
+        listener: (context, state){},
+        builder: (context, state){
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: lightTheme,
+            home: LoginScreen(),
+          );
+        },
+      ),
     );
   }
 }
