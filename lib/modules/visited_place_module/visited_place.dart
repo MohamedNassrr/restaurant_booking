45ec2bbd77d4891034df3019064800b65d7a54df
MohamedNassrr <mohamednasrrkh@gmail.com>
2023-03-13 00:48:33 +0200

    initial commit

---
+++ lib/modules/visited_place_module/visited_place.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VisitedPlaceScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Visited Place Screen',
+      ),
+    );
+  }
+}
